d5e3f52986bf2202ab5b809e2609fca49ba0a775
kunalsantoshpatil <158140309+kunalsantoshpatil@users.noreply.github.com>
2025-06-05 19:12:40 +0530

    Update Ex.java

--- src/Tests/Ex.java
+++ src/Tests/Ex.java
@@ -4,7 +4,7 @@ public class Ex {
 	public static void main(String[] args) {
 		System.out.println("KUnallllssdsdsl");
 		System.out.println("KUnallllssdsdsl");
-		System.out.println("KUnallllssdsdsl");
+	
 
 
 	}
