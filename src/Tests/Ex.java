e622b2447d97aca22b6778b06ff0bf9afda528e1
Kunal Patil <ytkunalpatil@gmail.com>
2025-06-05 19:10:24 +0530

    aded

--- src/Tests/Ex.java
+++ src/Tests/Ex.java
@@ -4,6 +4,8 @@ public class Ex {
 	public static void main(String[] args) {
 		System.out.println("KUnallllssdsdsl");
 		System.out.println("KUnallllssdsdsl");
+		System.out.println("KUnallllssdsdsl");
+
 
 	}
 
